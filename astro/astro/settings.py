206d48c5b31170c06802c0a10a62c032cdc6d250
laliteshx6 <2200031554@kluniversity.in>
2024-04-21 11:26:18 +0530

    fixed admin

--- astro/astro/settings.py
+++ astro/astro/settings.py
@@ -81,22 +81,22 @@ SECURE_CROSS_ORIGIN_OPENER_POLICY = "same-origin-allow-popups"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'astrology',  # database name
-        'USER': 'postgres',
-        'PASSWORD': '2004',
-        'HOST': 'localhost',
-        "PORT": '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'astrology',  # database name
+#         'USER': 'postgres',
+#         'PASSWORD': '2004',
+#         'HOST': 'localhost',
+#         "PORT": '5432',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
